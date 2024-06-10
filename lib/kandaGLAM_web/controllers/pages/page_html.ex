c03693b06c23ae0379ba220cac96d8ce956004b3
Chrisna Adhi Pranoto <chrisnaadhip@gmail.com>
2024-06-10 14:48:24 +0700

    trim title

--- lib/kandaGLAM_web/controllers/pages/page_html.ex
+++ lib/kandaGLAM_web/controllers/pages/page_html.ex
@@ -5,5 +5,25 @@ defmodule KandaGLAMWeb.PageHTML do
   See the `page_html` directory for all templates available.
   """
   use KandaGLAMWeb, :public_view
+  use Phoenix.LiveView
+
+  alias Phoenix.LiveView.JS
+
   import KandaGLAMWeb.Greeting
+
+  defp show_active_tab(js, to) do
+    js
+    |> JS.hide(to: "div.tab-content")
+    |> JS.show(
+      to: to,
+      transition: {"ease-out duration-500", "opacity-0", "opacity-100"},
+      time: 500
+    )
+  end
+
+  defp set_active_tab(js \\ %JS{}, tab) do
+    js
+    |> JS.remove_class("active-tab-item", to: "div.active-tab-item")
+    |> JS.add_class("active-tab-item", to: tab)
+  end
 end
